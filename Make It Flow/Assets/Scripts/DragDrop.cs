d65a0cd4a860b596df1698a536d0eb99a04d871e
Cal <98533461+HugeLadCal@users.noreply.github.com>
2024-07-09 09:18:47 +0100

    Unity
    Recreated the project in Unity due to it being superior for 2D games and have begun work on drag and drop elements for the UI.

---
+++ Make It Flow/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("OnPointerDown");
+    }
+}
